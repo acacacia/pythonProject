d652fe1fc948c9449cb2858604e677960acc2afe
zhanglei <744133564@qq.com>
2022-01-28 10:59:37 +0800

    202201028-1

--- sukodu.py
+++ sukodu.py
@@ -6,7 +6,7 @@ sudoku = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ['9', ' ', '2', '1', '4', ' ', ' ', ' ', ' '],
     [' ', '3', ' ', '8', '1', ' ', ' ', '7', '9'],
-    [' ', ' ', ' ', ' ', ' ', ' ', '5', '3', ' '],
+    ['8', ' ', ' ', ' ', ' ', '5', '3', ' ', ' '],
     [' ', '7', ' ', '2', '9', ' ', '8', '5', ' ']
 ]
 
@@ -68,7 +68,7 @@ def category2(su):
     return res
 
 
-def isNull(arr):
+def isNotNull(arr):
     i = 0
     res = []
     while i < len(arr):
@@ -78,8 +78,58 @@ def isNull(arr):
     return res
 
 
-value = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+def position(x, y):
+    if x < 3:
+        if y < 3:
+            return 0
+        if 2 < y < 6:
+            return 1
+        if 4 < y < 9:
+            return 2
+    if 2 < x < 6:
+        if y < 3:
+            return 3
+        if 2 < y < 6:
+            return 4
+        if 4 < y < 9:
+            return 5
+    if 4 < x < 9:
+        if y < 3:
+            return 6
+        if 2 < y < 6:
+            return 7
+        if 4 < y < 9:
+            return 8
+
+
+def numForNull(arr):
+    le = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == ' ':
+                le += 1
+    return le
+
+
+value = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
 convert = category(sudoku)
 grid = category2(sudoku)
+show(sudoku)
+count = 0
 
-print(set(isNull(sudoku[0]) + isNull(convert[0]) + isNull(grid[0])))
+sulen = numForNull(sudoku)
+
+while True:
+    if count == sulen:
+        break
+    for i in range(len(sudoku)):
+        for j in range(len(convert)):
+            if sudoku[i][j] == ' ':
+                res = value - (set(isNotNull(sudoku[i]) + isNotNull(convert[j]) + isNotNull(grid[position(i, j)])))
+                if len(res) == 1:
+                    sudoku[i][j] = list(res)[0]
+                    convert = category(sudoku)
+                    grid = category2(sudoku)
+                    count += 1
+
+show(sudoku)
